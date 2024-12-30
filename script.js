c14a71474e2524c06e36ac24c0781b2ee0a61c7a
Sumit6307 <sumitkesar6307@gmail.com>
2024-12-30 19:08:01 +0530

    Making Typed  Effect

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+var typed = new Typed(".auto-type", {
+       strings : [ "Full Stack Developer" ,"Web Developer" , "Web Designer", "API-Developer" , "Mobile Developer" , "CV Builder", "Portfolio Maker" , "Video Editor" , "Ethical Hacker " , "Trader" ,  "Entrepreneur"],
+
+    typeSpeed : 150,
+    backSpeed : 150,    
+    looped : true,
+    
+ })
+
